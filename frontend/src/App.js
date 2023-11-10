e6df239ab5b594255e28d9c6460b5498f8796ec3
Aruunce <118486133+Aruunce@users.noreply.github.com>
2023-11-10 20:41:10 +0200

    Added login, register pages and dropdown list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from './components/layout/header'
 import Footer from './components/layout/footer'
+
 import Home from './components/Home'
+
+import Login from './components/user/Login';
+import Register from './components/user/Register';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Home />
-           {/* <Route path="/" component={Home} exact /> */}
+          <div className="container container-fluid">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+          
+            </Routes>
+          </div>
         <Footer />
       </div>
     </Router>
