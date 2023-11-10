84996df1507ecb955e394a2a2a83f9cf2e9aa49e
Aruunce <118486133+Aruunce@users.noreply.github.com>
2023-11-10 18:45:21 +0200

    Adding necessary folders and Header/Footer

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react"
+
+const Home = () => {
+    return (
+        <Fragment>
+            <h1>Kažko trūksta</h1>
+        </Fragment>
+    )
+}
+
+export default Home
